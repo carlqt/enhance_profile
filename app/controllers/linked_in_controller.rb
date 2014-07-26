8f45c489b24bcee66c659ec9de40ffaabfc8dc82
Carl Tablante <carl.q.tablante@gmail.com>
2014-07-27 00:41:06 +0800

    facebook

--- app/controllers/linked_in_controller.rb
+++ app/controllers/linked_in_controller.rb
@@ -3,13 +3,16 @@ class LinkedInController < ApplicationController
     @code = params[:code]
     http = HTTParty.post("https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=#{@code}&redirect_uri=http://localhost:3000&client_id=#{PRIV[:li_key]}&client_secret=#{PRIV[:li_secret]}")
     access_token = http["access_token"]
-    xml_profile = HTTParty.get("https://api.linkedin.com/v1/people/~:(first-name,skills)?oauth2_access_token=#{access_token}")
+    xml_profile = HTTParty.get("https://api.linkedin.com/v1/people/~:(first-name,skills,positions)?oauth2_access_token=#{access_token}")
+    session[:li] = xml_profile
+    binding.pry
   end
 
   def facebook
     if env["omniauth.auth"]
       @profile = env["omniauth.auth"]
-      binding.pry
+      @facebook = Koala::Facebook::API.new(@profile.credentials.token)
+      @photos = @facebook.get_connections("me", "photos")
     end
   end
 
